7dcdd554166129092d9f8150ae7b8fdd20962053
playAndroid <playandroid@foxmail.com>
2016-06-07 23:07:45 +0800

    UI

--- wechattabandui/src/main/java/com/mystudy/wechattabandui/MainActivity.java
+++ wechattabandui/src/main/java/com/mystudy/wechattabandui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mystudy.wechattabandui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        ViewPager vp = (ViewPager) findViewById(R.id.vp);
     }
 }
